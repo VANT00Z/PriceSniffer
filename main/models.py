7fd01c929e82b67778e0fa5e42cc391abdeba981
huh <ivangalaktionov24@gmail.com>
2026-06-02 16:38:16 +0300

    Little update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30, null=False, blank=True)
+    email = models.EmailField(null=False, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta():
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        db_table = 'users'
